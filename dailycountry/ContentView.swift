c2372ccd8604071040307818db03d48373ec7af1
Esun4 <ethansun44@gmail.com>
2026-02-05 22:51:15 -0500

    Initial Commit

---
+++ dailycountry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dailycountry
+//
+//  Created by Ethan Sun on 2026-02-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dailycountry/dailycountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailycountryApp.swift
+//  dailycountry
+//
+//  Created by Ethan Sun on 2026-02-05.
+//
+
+import SwiftUI
+
+@main
+struct dailycountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
